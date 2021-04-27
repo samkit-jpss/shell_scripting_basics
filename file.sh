edf41cc3b2f4ff1e5602c5fcaaa9010ba1caa55d
Samkit Shah <shahsamkit35@gmail.com>
2021-04-27 11:05:45 +0530

    basic scripting

--- file.sh
+++ file.sh
@@ -5,26 +5,27 @@
 #unset x
 #echo "Welcome to ubuntu $x"
 
-#echo "File Name: $0"
-#echo "First Paramater: $1"
-#echo "Second Parameter: $2"
-#echo "Quoted Vlues: $@"
-#echo "Quoted values: $*"
-#echo "total number of paramaters: $#"
+#echo "File Name: $0" #RETRIEVE THE FIRST ARGUMENT FROM THE CMD (that is the file name)
+#echo "First Paramater: $1" #RETRIEVES SECOND PARAMETER
+#echo "Second Parameter: $2" # THIRD PARAMETER 
+#echo "Quoted Vlues: $@" #RETRIEVES ALL PARAMETER
+#echo "Quoted values: $*" 
+#echo "total number of paramaters: $#" #COUNT THE NUMBER OF PARAMETER 
 
-#arrays
+#ARRAYS
 
 #car[0]='skoda'
 #car[1]='honda'
 #car[2]='audi'
 #echo "${cars[*]}"
 
-echo "Enter a  value: "
-read x
-if [ "$x" -lt 10 ]
-then
-	echo "The value you'ev entered is $x which is less than 10"
-elif [ "$x" -gt 10 ]
-then 
-	echo "The value you've enterd is $x which is greater than 10"
-fi
+#CONDITIONS 
+#echo "Enter a  value: "
+#read x
+#if [ "$x" -lt 10 ]
+#then
+#	echo "The value you'ev entered is $x which is less than 10"
+#elif [ "$x" -gt 10 ]
+#then 
+#	echo "The value you've enterd is $x which is greater than 10"
+#fi
